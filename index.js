aa74cde77aae9303662b2ae044fdf51686cb177b
Sagar <Test>
2020-04-19 02:04:58 +0530

    Conversion to YAML and JSON

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let $ = require('jquery');
 console.log ("From the from Renderer -2");
 
 const newFieldBtn = document.getElementById('CreateBtn');
+const jsonBtn = document.getElementById('jsondwnld');
 
 newFieldBtn.addEventListener('click', function(event){
   console.log('Inside listner')
@@ -26,6 +27,60 @@ newFieldBtn.addEventListener('click', function(event){
   rootListDiv.appendChild(createObjNode(yamlData, "root"));
   $('.yamlList').hide();
 })
+
+jsonBtn.addEventListener ('click', function(event){
+  const yaml = require('js-yaml');  
+  let obj = new Object();
+
+    obj['root'] = createJSONObj($("#root").children());
+
+  // let childNodes = $("#root").children();
+  // if (childNodes){
+  //   let obj = new Object();
+  //   childNodes.each( function (index){
+  //     if ($(this).children('div').children('input').last().attr('class') === 'listItem'){
+  //     obj[$(this).children('div').children('input').first().attr('value')] = $(this).children('div').children('input').last().attr('value');
+  //     } else{
+
+  //     }
+  //   })
+  //   console.log(JSON.stringify(obj))
+  // } else { 
+  //   console.log("No root node found")
+  // }
+  console.log(JSON.stringify(obj.root));
+  console.log(yaml.safeDump(obj.root));
+})
+  function createJSONObj(nodeList){
+    if (nodeList){
+      let obj = new Object();
+      nodeList.each( function (index){
+        console.log($(this).children('div').first().attr('class') === 'listItem');
+        console.log($(this).children('div').children('input').last().attr('class'));
+        if ($(this).children('div').first().attr('class') === 'listItem'){
+          console.log("Inside if statement");
+        obj[$(this).children('div').children('input').first().attr('value')] = $(this).children('div').children('input').last().attr('value');
+        } else{
+          obj[$(this).children('div').children('input').first().attr('value')] = createJSONObj($(this).children('ul').first().children());
+        }
+      })
+    return obj;
+
+  }
+
+  // childNodes.each( function (index){
+
+  //   console.log( $(this).children('div').children('input').first());
+  //   console.log( $(this).children('div').children('input').first().attr('value'));
+  //   console.log( $(this).children('div').children('input').first().attr('class'));
+  //   console.log( $(this).children('div').children('input').last());
+  //   console.log( $(this).children('div').children('input').last().attr('value'));
+  //   console.log( $(this).children('div').children('input').last().attr('class'));
+  //  // console.log( index + ": " + $( this ).text() );
+  // })
+
+
+}
 
 
 function createObjNode(jsonObj, divId){
